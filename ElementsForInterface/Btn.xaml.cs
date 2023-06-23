107bf595f03fecd3ab3d7e29ff4ad423c78be4a8
Данила Хренов <danilahreno@gmail.com>
2023-06-23 13:26:10 +0300

    Добавил кнопку типчную

--- ElementsForInterface/Btn.xaml.cs
+++ ElementsForInterface/Btn.xaml.cs
@@ -10,18 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
-using LiveCharts;
-using LiveCharts.Wpf;
 
-namespace SunCloud.View
+namespace ElementsForInterface
 {
     /// <summary>
-    /// Логика взаимодействия для Main.xaml
+    /// Логика взаимодействия для Btn.xaml
     /// </summary>
-    public partial class Main : Window
+    public partial class Btn : UserControl
     {
-        public Main()
+        public Btn()
         {
             InitializeComponent();
         }
